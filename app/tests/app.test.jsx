885fe7ed3979175946fa703ada00e24feada8e4d
mjdude <mo@syncspot.net>
2016-06-06 12:20:16 -0700

    setup test framework

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,7 @@
+var expect = require('expect');
+
+// Second argument of it is a function, this this example im using ES6 arrow function as a shortcut
+
+it('it should properly run tests', () => {
+  expect(1).toBe(1);
+});
