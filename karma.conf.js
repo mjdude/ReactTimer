ee34809f66c9663e8215739e6b0fa6f2fd5f8d6c
mjdude <mo@syncspot.net>
2016-06-06 12:24:34 -0700

    fixed typo in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,19 @@
 var webpackConfig = require('./webpack.config.js');
-module.exports = function(config){
+
+module.exports = function (config) {
   config.set({
     browsers: ['Chrome'],
     singleRun: true,
-    framework: ['mocha'],
+    frameworks: ['mocha'],
     files: ['app/tests/**/*.test.jsx'],
     preprocessors: {
-      'app/tests/**/*.test.jsx' : ['webpack', 'sourcemap']
+      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
     },
     reporters: ['mocha'],
     client: {
-      mocha : {
+      mocha: {
         timeout: '5000'
-      },
+      }
     },
     webpack: webpackConfig,
     webpackServer: {
