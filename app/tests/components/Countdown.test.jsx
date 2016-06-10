7a144ea73c01b17b5766fb2bf7446a55af185914
mjdude <mo@syncspot.net>
2016-06-10 11:41:00 -0700

    added test for countdown

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+    it('should exist', () => {
+        expect(Countdown).toExist();
+    });
+});
