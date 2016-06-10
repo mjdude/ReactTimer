d0a05a4b21ff2a98b11919000f9d4a80bebcca4f
mjdude <mo@syncspot.net>
2016-06-09 18:33:13 -0700

    updated coundown component to actually countdown

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -7,12 +7,34 @@ var Countdown = React.createClass({
   getInitialState : function(){
     return {
       count: 0,
+      countdownStatus: 'stopped',
     };
+  },
+
+  componentDidUpdate: function(prevProps, prevState){
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch(this.state.countdownStatus){
+        case 'started' :
+          this.startTimer();
+          break;
+
+      }
+    }
+  },
+
+  startTimer: function(){
+      this.timer = setInterval(() => {
+        var newCount = this.state.count -1;
+        this.setState({
+          count: newCount >= 0 ? newCount : 0,
+        });
+      }, 1000);
   },
 
   handleSetCoundown: function(seconds){
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started',
     })
   },
 
