08ccc062f51f2d67a70becdaabffc3e0d863e3f2
mjdude <mo@syncspot.net>
2016-06-10 18:45:29 -0700

    updated timer page

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,11 +1,34 @@
 var React = require('react');
+var Clock = require('Clock');
+var Controls = require('Controls');
 
 var Timer = React.createClass({
+  getInitialState: function(){
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    }
+  },
+
+  handleStatusChange: function(newStatus){
+    this.setState({
+      countdownStatus: newStatus,
+    });
+  },
 
   render: function(){
+    var {count, countdownStatus} = this.state;
+
+    var renderControlArea = () => {
+      return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+
+    };
+
     return (
       <div>
-        <p>Timer.jsx</p>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}></Clock>
+        {renderControlArea()}
       </div>
     );
   }
